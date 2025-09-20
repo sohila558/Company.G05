dcb471c8a237a7f99310551e9598db8d9d14b55d
sohila558 <sohailat.441@gmail.com>
2025-09-20 23:55:06 +0300

    Create Create(Action - View)

--- Company.G05.PL/Controllers/DepartmentController.cs
+++ Company.G05.PL/Controllers/DepartmentController.cs
@@ -1,5 +1,7 @@
 ﻿using Company.G05.BLL.IRepositry;
 using Company.G05.BLL.Repositry;
+using Company.G05.DAL.Models;
+using Company.G05.PL.DTOs;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Company.G05.PL.Controllers
@@ -20,6 +22,35 @@ namespace Company.G05.PL.Controllers
             var departments = _departmentRepositry.GetAll();
 
             return View(departments);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateDepartmentDTO  model)
+        {
+            if (ModelState.IsValid) // Server Side Validation
+            {
+                var department = new Department()
+                {
+                    Code = model.Code,
+                    Name = model.Name,
+                    CreateAt = model.CreateAt
+                };
+
+                var Count = _departmentRepositry.Add(department);
+
+                if(Count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+
+            return View();
         }
     }
 }
