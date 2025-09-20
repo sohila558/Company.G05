a5b596185f4cf984de6c8b005d474a8a878eec24
sohila558 <sohailat.441@gmail.com>
2025-09-20 12:40:03 +0300

    Department Module BLL

---
+++ Company.G05.BLL/IRepositry/IDepartmentRepositry.cs
@@ -0,0 +1,19 @@
+﻿using Company.G05.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.G05.BLL.IRepositry
+{
+    public interface IDepartmentRepositry
+    {
+        IEnumerable<Department> GetAll();
+        Department? Get(int id);
+        int Add(Department model);
+        int Update(Department model);
+        int Delete(Department model);
+
+    }
+}
---
+++ Company.G05.BLL/Repositry/DepartmentRepositry.cs
@@ -0,0 +1,49 @@
+﻿using Company.G05.BLL.IRepositry;
+using Company.G05.DAL.Data.Contexts;
+using Company.G05.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.G05.BLL.Repositry
+{
+    internal class DepartmentRepositry : IDepartmentRepositry
+    {
+        private readonly CompanyDbContext _context;
+
+        public DepartmentRepositry()
+        {
+            _context = new CompanyDbContext();
+        }
+
+        public IEnumerable<Department> GetAll()
+        {
+            return _context.Departments.ToList();
+        }
+
+        public Department? Get(int id)
+        {
+            return _context.Departments.Find(id);
+        }
+
+        public int Add(Department model)
+        {
+            _context.Departments.Add(model);
+            return _context.SaveChanges();
+        }
+
+        public int Delete(Department model)
+        {
+            _context.Departments.Remove(model);
+            return _context.SaveChanges();
+        }
+
+        public int Update(Department model)
+        {
+            _context.Departments.Update(model);
+            return _context.SaveChanges();
+        }
+    }
+}
