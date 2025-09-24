c86e4707503132cbf7b290dcb4caa7129fa1cc30
sohila558 <sohailat.441@gmail.com>
2025-09-24 20:44:25 +0300

    Implementation Of BLL For Employees Services

---
+++ Company.G05.BLL/IRepositry/IEmployeeRepositry.cs
@@ -0,0 +1,18 @@
+﻿using Company.G05.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.G05.BLL.IRepositry
+{
+    public interface IEmployeeRepositry
+    {
+        IEnumerable<Employee> GetAll();
+        Employee? Get(int id);
+        int Add(Employee model);
+        int Update(Employee model);
+        int Delete(Employee model); 
+    }
+}
